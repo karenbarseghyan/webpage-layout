de36a33d48eebd1ae1656c4748611e4152ff470c
karenbarseghyan <karenbarseghyan90@gmail.com>
2020-12-02 18:59:31 +0400

    User-search Functionality

--- src/containers/views/users/Users.jsx
+++ src/containers/views/users/Users.jsx
@@ -1,12 +1,43 @@
-import { TextField, InputAdornment } from '@material-ui/core';
+import { TextField, InputAdornment, Card } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import React, {useState} from 'react';
+import {SEARCH_USERS} from '../../url';
+import axios from'axios';
 
 
 const Users = () => {
-    const [value,setValue] = useState('')
+    const [value,setValue] = useState('');
+    const [data,setData] = useState([])
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        searchUsers()
+    }
+
+    const searchUsers = async() => {
+        try {
+            const response = await axios.get(SEARCH_USERS, {
+                params:{
+                    q:`${value.trim()} type:user`,
+                    per_page: 10,
+                }}); 
+            const data = await response.data;
+            setData(data.items);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+const mapUsers = () => {
+    return data?.map((item, ind) => {
+        return <Card key = {item.id}>{item.login}</Card>
+    })
+}
+
     return (
-       <>
+        <>
+       <form onSubmit={handleSubmit}>
             <TextField
             label ="Search Users"
             name = "userSearch"
@@ -21,6 +52,10 @@ const Users = () => {
             onChange = {(ev) => { setValue(ev.target.value)}}
             value = {value}
             />
+       </form>
+       <Card>
+           {mapUsers()}
+       </Card>
        </>
     );
 }
