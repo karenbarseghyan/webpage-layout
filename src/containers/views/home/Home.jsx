6567df3b5534ef297afa50a733cb87e0c8696306
karenbarseghyan <karenbarseghyan90@gmail.com>
2020-12-10 22:24:53 +0400

    Creating increase-decrease buttons with hooks in home-page

--- src/containers/views/home/Home.jsx
+++ src/containers/views/home/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import { IconButton } from '@material-ui/core';
+import { AddCircle, RemoveCircle } from '@material-ui/icons';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [counter, setCounter] = useState (0);
     return (
-        <h1>Home</h1>
+        <>
+            <IconButton onClick = {()=>{setCounter(counter-1)}}>
+                <RemoveCircle />
+            </IconButton>
+            <h3>
+                {counter}
+            </h3>
+            <IconButton onClick = {()=>{setCounter(counter+1)}}>
+                <AddCircle />
+            </IconButton>
+        </>
     );
 }
 
